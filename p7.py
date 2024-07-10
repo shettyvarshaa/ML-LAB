82b4fa78cd152f17bf92eb3774a0cef1cab0efa3
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-07-10 11:44:25 +0530

    Made changes in p7

--- p7.py
+++ p7.py
@@ -2,12 +2,12 @@ from sklearn.datasets import fetch_20newsgroups_vectorized
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
-# Load dataset
+
 newsgroups = fetch_20newsgroups_vectorized()
+
 X_train, X_test, y_train, y_test = train_test_split(newsgroups.data, newsgroups.target, test_size=0.2, random_state=42)
-# Train classifier
 nb_classifier = MultinomialNB().fit(X_train, y_train)
-# Predict and evaluate
 y_pred = nb_classifier.predict(X_test)
+
 print(f"Accuracy: {accuracy_score(y_test, y_pred)}\n")
 print(f"Classification Report:\n{classification_report(y_test, y_pred, target_names=newsgroups.target_names)}")
