72eec85b5ec6a891c389eacf8480146f07ddb5a1
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-07-10 11:42:13 +0530

    made changes in p8

--- p8.py
+++ p8.py
@@ -17,15 +17,12 @@ column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'c
 df = pd.read_csv(url, header=None, names=column_names)
 X = df.drop('class', axis=1)
 y = df['class']
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = GaussianNB()
-model.fit(X_train, y_train)
+model = GaussianNB().fit(X_train, y_train)
 y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred)
-print(f"Accuracy: {accuracy}\n")
-print("Classification Report:")
-print(report)
+print(f"Accuracy:{accuracy_score(y_test, y_pred)}\n")
+print(f"Classification Report:\n{classification_report(y_test, y_pred)}")
 
 
 
