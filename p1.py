45f0316d7a09c19b83c6e45ecefaf7b1c3c50e1a
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-07-29 21:37:08 +0530

    Modified P2 and P1

--- p1.py
+++ p1.py
@@ -1,21 +1,21 @@
 import csv
-a = []
 with open('enjoysport.csv', 'r') as csvfile:
-    for row in csv.reader(csvfile):
-        a.append(row)
-    print(a)
-print("\n The total number of training instances are : ",len(a))
-num_attribute = len(a[0])-1
-print("\n The initial hypothesis is : ")
-hypothesis = ['0']*num_attribute
-print(hypothesis)
-for i in range(0, len(a)): 
-    if a[i][num_attribute] == '1':
-        for j in range(0, num_attribute):   
-            if hypothesis[j] == '0' or hypothesis[j] == a[i][j]:
-                hypothesis[j] = a[i][j]
+    data = list(csv.reader(csvfile))
+print(data)
+
+print("\nThe total number of training instances are:", len(data))
+
+num_attributes = len(data[0]) - 1
+hypothesis = ['0'] * num_attributes
+print("\nThe initial hypothesis is:", hypothesis)
+
+for i, instance in enumerate(data):
+    if instance[num_attributes] == 'yes':
+        for j in range(num_attributes):
+            if hypothesis[j] == '0' or hypothesis[j] == instance[j]:
+                hypothesis[j] = instance[j]
             else:
                 hypothesis[j] = '?'
-    print("\n The hypothesis for the training instance {} is :\n" .format(i+1),hypothesis)
-print("\n The Maximally specific hypothesis for the training instance is ")
-print(hypothesis)
+    print(f"\nThe hypothesis after training instance {i + 1} is:", hypothesis)
+
+print("\n\nThe Maximally specific hypothesis is:", hypothesis)
---
+++ p2.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('enjoysport.csv')
+concepts = data.iloc[:, :-1].values
+target = data.iloc[:, -1].values
+
+def learn(concepts, target):
+    specific_h = concepts[0].copy()
+    general_h = [['?' for _ in range(len(specific_h))] for _ in range(len(specific_h))]
+
+    for i, h in enumerate(concepts):
+        if target[i] == "yes":
+            for x in range(len(specific_h)):
+                if h[x] != specific_h[x]:
+                    specific_h[x] = '?'
+                    general_h[x][x] = '?'
+        else:
+            for x in range(len(specific_h)):
+                if h[x] != specific_h[x]:
+                    general_h[x][x] = specific_h[x]
+                else:
+                    general_h[x][x] = '?'
+
+    general_h = [h for h in general_h if h != ['?' for _ in range(len(specific_h))]]
+    return specific_h, general_h
+
+s_final, g_final = learn(concepts, target)
+print("\nFinal Specific_h:", s_final)
+print("\nFinal General_h:", g_final)
