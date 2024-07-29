92c85ca0fd6170d79969e965ee34789ff9aaf2de
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-07-29 22:37:40 +0530

    Added 10th program

---
+++ p10.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+iris = load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=3).fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+print(f"Accuracy: {accuracy_score(y_test, y_pred)}\n")
+print(f"Confusion Matrix:\n{confusion_matrix(y_test, y_pred)}\n")
+print(f"Classification Report:\n{classification_report(y_test, y_pred)}")
