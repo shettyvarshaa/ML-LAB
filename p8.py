5890e4c074bec7756027d1b86ccaf6508b1dfcde
shettyvarshaa <shettyvarshaa275@gmail.com>
2024-07-30 01:26:50 +0530

    removed unwanted logic in p8:

--- p8.py
+++ p8.py
@@ -1,21 +1,19 @@
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report
 
-iris = load_iris()
 column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
-df = pd.read_csv("./iris.csv", header=None, names=column_names)
+df = pd.read_csv("iris.csv", header=None, names=column_names)
 X = df.drop('class', axis=1)
 y = df['class']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = GaussianNB().fit(X_train, y_train)
 y_pred = model.predict(X_test)
-print(f"Accuracy:{accuracy_score(y_test, y_pred)}\n")
+
+print(f"Accuracy: {accuracy_score(y_test, y_pred)}\n")
 print(f"Classification Report:\n{classification_report(y_test, y_pred)}")
 
-# Without csv file
-# csv_file_path = 'iris.csv'
-# df.to_csv(csv_file_path, index=False)
+# Optional: Save the DataFrame to a CSV file (if needed for future use)
+# df.to_csv('iris.csv', index=False)
